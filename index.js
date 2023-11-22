b16ccb104ec82ec5593c0d9901cdd5699335b9d4
Jack Anderson <jaandersonck@gmail.com>
2023-11-22 12:51:08 -0500

    add jquery

--- index.js
+++
@@ -1,5 +0,0 @@
-const jack_image_list = ["IMG_2247.jpg", "IMG_1572.jpg", "IMG_9390.jpg"]
-const jack_image = jack_image_list[Math.floor(Math.random() * jack_image_list.length)]
-
-document.onload()
-document.getElementById("jack-img").src = jack_image
